29a8466ea307b867483853fedcf1582e33a1dd16
EGRim-2009 <egoar@ftstudent.org>
2024-11-21 13:46:28 -0500

    nothing  new
    changed enemy speed

--- node_2d.gd
+++ node_2d.gd
@@ -1,16 +1,16 @@
 extends Node2D
-class_name enemy
+class_name Enemy
 
 @onready var detection_range = $Range
-@export var _Enemy_SPEED = 300.0
+@export var _Enemy_SPEED = 55
 
 
 
 
 
-func _physics_process(_delta):
-	var bodies = detection_range.get_overlapping_bodies()
-	for body in bodies:
-		look_at(body.global_position)
+#func _physics_process(_delta):
+	#var bodies = detection_range.get_overlapping_bodies()
+	#for body in bodies:
+		#look_at(body.global_position)
 # Called when the node enters the scene
 # Called when the node enters the scene tree for the first time.
