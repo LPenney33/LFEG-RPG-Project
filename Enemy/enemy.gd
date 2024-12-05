78418ced4f8e87f5a51e4ce8d6742d98b001f3fb
EGRim-2009 <egoar@ftstudent.org>
2024-12-05 13:23:05 -0500

    enemy movement

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,3 +1,9 @@
 extends Enemy
 
 var Enemy_SPEED = 55
+#@onready var target =
+#func _physics_process(delta):
+	#var direction = (target.position- position).normalized()
+	#velocity= direction * speed
+	#look_at(target.position)
+	#move_and_slide()
