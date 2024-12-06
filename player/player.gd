2bf319cd803a8d99e34d316a1fb653b9c142126b
EGRim-2009 <egoar@ftstudent.org>
2024-12-06 14:00:56 -0500

    player movement
    better player movement
    basic attack

--- player/player.gd
+++ player/player.gd
@@ -1,13 +1,12 @@
 extends CharacterBody2D
 
 @export var speed = 100
-@export var sprint_speed = 450
+@export var sprint_speed = 200
 @onready var sprite = $AnimatedSprite2D
 var target = position
 var isAttacking = false
-enum Directions{ LEFT,RIGHT}
-var facing: Directions = Directions.RIGHT
-####continue later
+enum Directions{UP,DOWN,LEFT,RIGHT}
+var facing : Directions = Directions.DOWN
 
 func _process(_delta):
 	print(isAttacking)
@@ -16,19 +15,39 @@ func _process(_delta):
 		target = get_global_mouse_position()
 	velocity.x = direction.x * speed
 	velocity.y = direction.y * speed
-#fix code
-	if direction.x ==0 && direction.y ==0 && isAttacking == false:
+	#fix code
+	if direction.x ==0 && direction.y ==0 && !isAttacking:
 		sprite.play("idle")
 
-	if direction.x > 0 && isAttacking== false:
+	if direction.x > 0 && !isAttacking:
 		sprite.play("right")
 		sprite.flip_h = false
 		facing = Directions.RIGHT
 
-	if direction.x < 0 && isAttacking == false:
+	if direction.x < 0 && !isAttacking:
 		sprite.play("right")
 		sprite.flip_h = true
 		facing = Directions.LEFT
+
+	if direction.y < 0 && !isAttacking:
+		sprite.play("up")
+		sprite.flip_h = false
+		facing = Directions.UP
+
+	if direction.y > 0 && !isAttacking:
+		sprite.play("down")
+		sprite.flip_h = false
+		facing = Directions.DOWN
+
+	if Input.is_action_just_pressed("attack"):
+		isAttacking = true
+		if facing == Directions.LEFT && isAttacking :
+			sprite.play("attack_right")
+			sprite.flip_h = true
+
+		if facing == Directions.RIGHT && isAttacking :
+			sprite.play("attack_right")
+			sprite.flip_h = false
 
 	if Input.is_action_pressed("sprint") && !isAttacking:
 		velocity = velocity.normalized() * sprint_speed
@@ -38,11 +57,11 @@ func _process(_delta):
 	if Input.is_action_just_pressed("attack"):#.is_action_pressed("attack"):
 		isAttacking = true
 		if facing == Directions.LEFT && isAttacking :
-			sprite.play("attack")
+			sprite.play("attack_right")
 			sprite.flip_h= true
 
 		if facing == Directions.RIGHT && isAttacking :
-			sprite.play("attack")
+			sprite.play("attack_right")
 			sprite.flip_h= false
 
 func _physics_process(_delta):
@@ -51,7 +70,7 @@ func _physics_process(_delta):
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	print("animation finished")
-	if (sprite.animation == "attack"):
+	if (sprite.animation == "attack_right"):
 		isAttacking = false
 		sprite.play("idle")
+		pass # Replace with function body.
