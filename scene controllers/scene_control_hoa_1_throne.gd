a5cd0e2b04d4c50e5252b35aee9b8935d08566a4
LPenney33 <lpenney@ftstudent.org>
2024-12-09 14:48:18 -0500

    HOA 2 Teleports Finished

--- scene controllers/scene_control_hoa_1_throne.gd
+++ scene controllers/scene_control_hoa_1_throne.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 
-func _on_hoa_1outside_teleport_child_entered_tree(body):
+func _on_hoa_1hall_teleport_child_entered_tree(body):
 	if body.name == ("player"):
 		get_tree().change_scene_to_file("res://scenes/hoa_1_main_hall.tscn")
